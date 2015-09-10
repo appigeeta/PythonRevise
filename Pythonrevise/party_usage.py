5893824b547bb912f627af2ef156b7202953a188
appigeeta <appigeeta@gmail.com>
2015-09-10 16:14:10 -0700

    Birthday planning details.

---
+++ Pythonrevise/party_usage.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+from party import party_planning
+badyParty=party_planning("10/11","lunch")
+badyParty.att("50")
+badyParty.place_sele("Clubhouse")
+badyParty.food("Pizza(veg)")
+badyParty.type("2nd birthday party of Amogh")
+badyParty.party_details()
